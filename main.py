0b24e81e87b5770b8b40702943d8dd956c29574f
glmontano <67193952+glmontano@users.noreply.github.com>
2023-01-12 04:40:17 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from pytube import YouTube, exceptions # Used to stream YT
 from pydub import AudioSegment # Used for audio conversion
 from io import BytesIO # Used for memory storage
 
+# Example: Lion King's 'Remember Who You Are'
+YOUTUBE_URL = 'https://www.youtube.com/watch?v=FNBAmsriyY8'
+
 try:
   # Stream and download audio
-  yt = YouTube(YOUR_YOUTUBE_URL)
+  yt = YouTube(YOUTUBE_URL)
   audio = yt.streams.filter(only_audio=True).first() # Stream audio
   audio_bytes = BytesIO() # Create 
   audio.stream_to_buffer(audio_bytes)
@@ -14,6 +17,8 @@ try:
   sound = AudioSegment.from_file(file=audio_bytes).export(BytesIO(), 'mp3')
   with open('path/to/file', 'wb') as f:
     f.write(sound.getbuffer())
+    
+# Exception for inappropriate URL
 except exceptions.RegexMatchError:
   print("Inappropriate URL")
   
